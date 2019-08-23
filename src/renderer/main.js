5c89126d5da3962166c820f553f1bb2613a5dfb4
goodjun <goodjun@163.com>
2019-08-22 19:08:50 -0700

    feat: 引入codemirror iview

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -5,13 +5,24 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
+
+import VueCodemirror from 'vue-codemirror'
+import 'codemirror/lib/codemirror.css'
+
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
+Vue.use(iView)
+Vue.use(VueCodemirror)
+
 /* eslint-disable no-new */
 new Vue({
-  components: { App },
+  components: {
+    App
+  },
   router,
   store,
   template: '<App/>'
