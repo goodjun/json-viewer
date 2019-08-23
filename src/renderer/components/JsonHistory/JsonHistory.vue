b099ad6a48724efafe907b9b7b8a6ecd267833fd
goodjun <goodjun@163.com>
2019-08-22 19:45:05 -0700

    feat: 添加JsonHistory组件

---
+++ src/renderer/components/JsonHistory/JsonHistory.vue
@@ -0,0 +1,33 @@
+<template>
+  <Card title="History" icon="ios-apps" :padding="0" shadow>
+    <CellGroup>
+      <Cell v-for="history in historys" :title="history.json" :label="history.datetime" />
+    </CellGroup>
+  </Card>
+</template>
+
+<script>
+export default {
+  name: "json-history",
+  data() {
+    return {
+      historys: [
+        { json: '{"name": "tom"}', datetime: "2019/08/22 19:39" },
+        { json: '{"name": "tom"}', datetime: "2019/08/22 19:39" },
+        { json: '{"name": "tom"}', datetime: "2019/08/22 19:39" },
+        { json: '{"name": "tom"}', datetime: "2019/08/22 19:39" },
+        { json: '{"name": "tom"}', datetime: "2019/08/22 19:39" },
+        { json: '{"name": "tom"}', datetime: "2019/08/22 19:39" },
+        { json: '{"name": "tom"}', datetime: "2019/08/22 19:39" }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+.ivu-card {
+  margin-right: 10px;
+  height: 520px;
+}
+</style>
