b6aed4f486ec7f6a87cf49d3b2ab7a0c4af7a45b
goodjun <goodjun@163.com>
2019-08-26 00:56:11 -0700

    feat: 引入vue uuid

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -10,6 +10,8 @@ import 'iview/dist/styles/iview.css'
 
 import moment from 'moment'
 
+import UUID from 'vue-uuid';
+
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
@@ -17,6 +19,8 @@ Vue.config.productionTip = false
 Vue.use(iView)
 
 Vue.prototype.$moment = moment;
+
+Vue.use(UUID);
 
 /* eslint-disable no-new */
 new Vue({
